6a2f804d549d4f6deb4da30f97ef8f4eed662387
Steven Parkes <smparkes@smparkes.net>
2010-03-03 13:45:28 -0800

    wip

--- spec/example/hello_world_spec.js
+++ spec/example/hello_world_spec.js
@@ -1,29 +1,21 @@
-jasmine.include(jazrb_root + "/spec/example/spec_helper.js");
-
+"use strict";
 (function($){
-
   describe("hello_world",function(){
-    
     describe("contents",function(){
-
       var loops = 500000;
-
       for(var i = 0; i < 10; i++ ) {
+        (function(){
+          it("should contain the text 'hello, world'",function(){
+            expect($("div:contains('hello, world')").size()).toBeGreaterThan(0);
+            for(var j = 0; j<loops; j++) {}
+          });
 
-        it("should contain the text 'Hello, World'",function(){
-          expect($("div:contains('Hello, World')").size()).toBeGreaterThan(0);
-          for(var j = 0; j<loops; j++);
-        });
-
-        it("should contain the text 'Hello, World'",function(){
-          expect($("div:contains('Hello, World')").size()).toBeGreaterThan(0);
-          for(var j = 0; j<loops; j++);
-        });
-
+          it("should contain the text 'hello, world'",function(){
+            expect($("div:contains('hello, world')").size()).toBeGreaterThan(0);
+            for(var j = 0; j<loops; j++) {}
+          });
+        }());
       }
-
     });
-
   });
-
-})(jQuery);
+}(jQuery));
