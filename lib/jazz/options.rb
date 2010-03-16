18565a11c0f9f3937a5209f72b91141c5f848530
Steven Parkes <smparkes@smparkes.net>
2010-03-16 15:22:05 -0700

    baby steps towards frameworks and drivers

--- lib/jazz/options.rb
+++ lib/jazz/options.rb
@@ -4,8 +4,15 @@ $jazz_verbose = false
 $jazz_xml = false
 $jazz_deps = nil
 $jazz_qunit = false
-$jazz_envjs = false
+$jazz_jasmine = false
 $jazz_wake = false
+$jazz_envjsrb = false
+$jazz_firefox = false
+$jazz_chrome = false
+$jazz_chromium = false
+$jazz_ie = false
+$jazz_safari = false
+$jazz_testflock = false
 
 OptionParser.new do |o|
 
@@ -27,6 +34,42 @@ OptionParser.new do |o|
 
   o.on("--wake") do
     $jazz_wake = true
+  end
+
+  o.on("--wake") do
+    $jazz_wake = true
+  end
+
+  o.on("--wake") do
+    $jazz_wake = true
+  end
+
+  o.on("--wake") do
+    $jazz_wake = true
+  end
+
+  o.on("--chomium") do
+    $jazz_chomium = true
+  end
+
+  o.on("--chrome") do
+    $jazz_chrome = true
+  end
+
+  o.on("--ie") do
+    $jazz_ie = true
+  end
+
+  o.on("--testflock") do
+    $jazz_testflock = true
+  end
+
+  o.on("--safari") do
+    $jazz_safari = true
+  end
+
+  o.on("--firefox") do
+    $jazz_firefox = true
   end
 
 end.parse!
