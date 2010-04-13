f98eac21c1a1ce48cbaa194f21f5b9b4422f595a
Steven Parkes <smparkes@smparkes.net>
2010-04-13 13:22:12 -0700

    shut up jslint

--- spec/example/hello_world_spec.js
+++ spec/example/hello_world_spec.js
@@ -2,19 +2,21 @@
 (function($){
   describe("hello_world",function(){
     describe("contents",function(){
-      var loops = 500000;
-      for(var i = 0; i < 10; i++ ) {
-        (function(){
-          it("should contain the text 'hello, world'",function(){
-            expect($("div:contains('hello, world')").size()).toBeGreaterThan(0);
-            for(var j = 0; j<loops; j++) {}
-          });
+      var loops = 50000;
+      var fn = function(){
+        it("should contain the text 'hello, world'",function(){
+          expect($("div:contains('hello, world')").size()).toBeGreaterThan(0);
+          for(var j = 0; j<loops; j++) {}
+        });
 
-          it("should contain the text 'hello, world'",function(){
-            expect($("div:contains('hello, world')").size()).toBeGreaterThan(0);
-            for(var j = 0; j<loops; j++) {}
-          });
-        }());
+        it("should contain the text 'hello, world'",function(){
+          expect($("div:contains('hello, world')").size()).toBeGreaterThan(0);
+          for(var j = 0; j<loops; j++) {}
+        });
+      };
+      
+      for(var i = 0; i < 10; i++ ) {
+        fn();
       }
     });
   });
